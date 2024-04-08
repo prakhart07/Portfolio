ecbe6717e1d4b2a5314db2173a9f281b8ef47c55
prakhart07 <prakhart073@gmail.com>
2024-04-09 00:20:24 +0530

    txt

--- portfolio/src/aboutme.js
+++ portfolio/src/aboutme.js
@@ -19,11 +19,11 @@ function AboutMe(){
                     <h6>Full-Stack Developer</h6>
                     </h2>
                     
-                    <p className='sidebar-listItemText'>Hello! I'm Prakhar, a dedicated software developer with a passion for crafting efficient and innovative solutions to complex problems. I thrive in the world of code and enjoy turning ideas into reality.</p>
+                    <p>Hello! I'm Prakhar, a dedicated software developer with a passion for crafting efficient and innovative solutions to complex problems. I thrive in the world of code and enjoy turning ideas into reality.</p>
 
                             {/* With [X years/months] of experience in the software development industry, */}<p className='sidebar-listItemText'>I've had the opportunity to work on a variety of projects, ranging from web pages and web applications to backend systems. My expertise includes HTML & CSS,JS,React.JS,J2E,.NET,MySQL,DevOps,Git etc, and I'm always eager to embrace new challenges and technologies.</p> 
 
-                            <p className='sidebar-listItemText'>I believe in the power of collaboration and open-source contributions, and I actively engage with the developer community to learn and share knowledge. When I'm not coding, you can find me exploring the latest tech trends, experimenting with personal projects, or watching animes and shows.</p> 
+                            <p>I believe in the power of collaboration and open-source contributions, and I actively engage with the developer community to learn and share knowledge. When I'm not coding, you can find me exploring the latest tech trends, experimenting with personal projects, or watching animes and shows.</p> 
 
                             <p className='sidebar-listItemText'>I'm driven by a constant desire to learn, grow, and create meaningful software that makes a positive impact. If you have an exciting project or collaboration opportunity in mind, or simply want to connect and discuss tech, feel free to reach out. Let's code, innovate, and build a better digital world together!</p>
 
